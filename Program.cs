1f68d11c066e6243186caff82121b0c45f586ff2
Marllon Drion <marllondrion@gmail.com>
2024-06-05 23:05:28 +1000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 //     options.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("en");
 //     options.SupportedCultures = supportedCultures;
 //     options.SupportedUICultures = supportedCultures;
-});
+// });
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
